aa7c949ef98e28cf36bae66596cde8337749e0ac
Jan Roman Cisowski <janromancisowski@cisowscy.com>
2017-10-18 05:42:33 +0200

    Update MAIN.js

--- src/assets/js/MAIN.js
+++ src/assets/js/MAIN.js
@@ -17,7 +17,7 @@ $(function () {
     $("#div0")
         .append("<h2>" + tytulStrony1 + "</h2>")
         .append("<hr>")
-        .append("<h4>" + tytulStrony2 + "</h4>");
+        .append("<h5>" + tytulStrony2 + "</h5>");
     $("#div1").append("<h4>Metoda</h4>");
     $("#div2").append("<h4>Przyszła<br>Aplikacja</h4>");
     $("#div3").append("<h4>Nazwy<br>Stosunków</h4>");    
@@ -27,7 +27,7 @@ $(function () {
 function pozycja_el() {
     let rem7 = 7*16;
     let rem9 = 9*16;
-    let rem41 = 33*16;
+    let rem41 = 30*16;
     let Hq = ($("body").innerWidth()/4);
     let Vq = ($("body").innerHeight()/4);
     //if (rem41+rem9>)
@@ -39,22 +39,22 @@ function pozycja_el() {
     $("#div1")
         .css({
             left: Math.floor(3*Hq-rem9/2), 
-            top: Math.floor(Vq-rem9/2)
+            top: Math.floor(Vq-rem7/2)
         });
     $("#div2")
         .css({
             left: Math.floor(3*Hq-rem9/2), 
-            top: Math.floor(3*Vq-rem9/2)
+            top: Math.floor(3*Vq-rem7/2)
         });
     $("#div3")
         .css({
             left: Math.floor(Hq-rem9/2), 
-            top: Math.floor(3*Vq-rem9/2)
+            top: Math.floor(3*Vq-rem7/2)
         });
     $("#div4")
         .css({
             left: Math.floor(Hq-rem9/2), 
-            top: Math.floor(Vq-rem9/2)
+            top: Math.floor(Vq-rem7/2)
         });
     
 }
